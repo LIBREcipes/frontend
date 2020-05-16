9ee918775b0a9cd17a7f6a6a396a3a86bd2feeb2
Matthias De Bie <mattydebie@gmail.com>
2020-05-17 00:04:44 +0200

    add footer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,20 @@
-import { Component } from '@angular/core';
+import { Component } from '@angular/core'
+import { Title } from '@angular/platform-browser'
+import { environment } from 'src/environments/environment'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.sass']
+  styleUrls: ['./app.component.sass'],
 })
 export class AppComponent {
-  title = 'librecipes';
-  user = {
-    'username': 'mattydebie',
+  title = 'librecipes'
+
+  constructor() {
+    this.title = environment.config.title
+  }
+
+  get version(): string {
+    return environment.version
   }
 }
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,6 +2,7 @@ import DynamicEnvironment from './dynamic-environment'
 
 class Environment extends DynamicEnvironment {
   public production: boolean
+  public version = '0.1a'
 
   constructor() {
     super()
